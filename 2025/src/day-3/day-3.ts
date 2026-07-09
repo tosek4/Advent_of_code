a3b6428c187686c1e7cfe1d3dc8a69a5ee43b666
TosoK <tosekleckaroski@gmail.com>
2026-07-09 12:49:04 +0200

    feat: solve day 3

--- 2025/src/day-3/day-3.ts
+++ 2025/src/day-3/day-3.ts
@@ -1,8 +1,20 @@
-
 import { readFile } from "./readData";
 
 export const Day3 = async () => {
-const dataRead = "src/day-3/data/data.txt";
-  const formatData = (await readFile(dataRead))
-    return 0
-}
+  const dataRead = "src/day-3/data/data.txt";
+  const formatData = (await readFile(dataRead)).split("\n");
+  let sum = 0;
+  for (const line of formatData) {
+    let maxNum = 0;
+    for (let i = 0; i < line.length; i++) {
+      for (let j = i + 1; j < line.length; j++) {
+        const num = parseInt(line[i]) * 10 + parseInt(line[j]);
+        if (num > maxNum) {
+          maxNum = num;
+        }
+      }
+    }
+    sum += maxNum;
+  }
+  return sum;
+};
