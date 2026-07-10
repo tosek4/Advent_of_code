62280917c00057a457552e9097f9e3aef7eb395c
TosoK <tosekleckaroski@gmail.com>
2026-07-11 01:40:43 +0200

    feat: solve day 6 plus

--- 2025/src/day-6/day-plus-6.ts
+++ 2025/src/day-6/day-plus-6.ts
@@ -1,8 +1,51 @@
-
 import { readFile } from "./readData";
 
 export const DayPlus6 = async () => {
   const dataRead = "src/day-6/data/data.txt";
-  const formatData = (await readFile(dataRead))
-    return 0
-}
+  const data = (await readFile(dataRead)).split("\n");
+
+  let sum = 0;
+  let tmpSum = 0;
+  let op = "";
+
+  for (let i = 0; i < data[0].length; i++) {
+    let tmpNum = 0;
+    for (let j = 0; j < data.length - 1; j++) {
+      let num = parseInt(data[j][i]);
+
+      const current = data[data.length - 1][i];
+
+      if (current === "+" || current === "*") {
+        op = current;
+      }
+
+      if (tmpNum === 0 && !Number.isNaN(num)) {
+        tmpNum = num;
+      } else if (!Number.isNaN(num)) {
+        tmpNum = tmpNum * 10 + num;
+      }
+    }
+
+    if (tmpNum === 0) {
+      sum += tmpSum;
+      tmpSum = 0;
+      continue;
+    }
+
+    if (op === "+") {
+      tmpSum += tmpNum;
+    } else if (op === "*") {
+      if (tmpSum === 0) {
+        tmpSum += tmpNum;
+      } else {
+        tmpSum = tmpSum * tmpNum;
+      }
+    }
+
+    if (i + 1 === data[0].length) {
+      sum += tmpSum;
+    }
+  }
+
+  return sum;
+};
